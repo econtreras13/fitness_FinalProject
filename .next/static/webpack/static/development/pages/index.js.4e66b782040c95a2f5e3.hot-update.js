30361ea9188390c9b8e13db4c9238b161c9accf7
Evelin Contreras <evelinc1113@gmail.com>
2019-12-11 03:52:53 -0600

    uploading final project

---
+++ .next/static/webpack/static/development/pages/index.js.4e66b782040c95a2f5e3.hot-update.js
@@ -0,0 +1,33 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "C:\\Users\\muham\\Documents\\GitHub\\fitness_FinalProject\\pages\\index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+var SignupPage = function SignupPage() {
+  return __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "hello");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Index);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.4e66b782040c95a2f5e3.hot-update.js.map
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # fitness_FinalProject
+
+Hello!
+
+This web app is for people who are into fitness to sign up and showcase their skills! 
+
+A person can sign up and link their instagram account.
+Users can veiw the application and see what sorts of cool stuff people can do in the world!
+
+Tech info:
+
+This app is built on the following:
+1. HTML
+2. ReactJS
+3. NodeJS
+4. Utilizes JSON data 
+
+For storing jsons, I used this online tool called myjson-
+
+http://myjson.com/
+
+This site was useful for using test data as you can see in the test json.
+
